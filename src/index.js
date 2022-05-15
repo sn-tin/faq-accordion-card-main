a6e800d1ce0e8c29fc183d15389fbacc635f03fa
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-05-15 20:31:19 +0800

    Final working javascript code

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ const boxIllustration = document.querySelector(".box-illust");
 
 accordionQuestions.forEach(questions => {
     questions.addEventListener('click', function() {
-    
-        // accordionQuestions.forEach(itm => itm.classList.remove("active"));
-        // this.classList.toggle("active");    
-
-        // if (questions.classList.contains("active")) { 
-        //     boxIllustration.classList.add("move-box")
-        // } else {
-        //     boxIllustration.classList.remove("move-box")
-
-        // }
         
         // To show and hide answers on FAQs
         this.classList.toggle("active");
