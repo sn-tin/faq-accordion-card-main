ba6d87f64b632c11d06cc4aedfe545fb6eb17bba
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-05-15 20:35:45 +0800

    Add class move-box when question is clicked

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -122,21 +122,18 @@ var accordionQuestions = document.querySelectorAll(".faqs");
 var boxIllustration = document.querySelector(".box-illust");
 accordionQuestions.forEach(function (questions) {
   questions.addEventListener('click', function () {
-    // accordionQuestions.forEach(itm => itm.classList.remove("active"));
-    // this.classList.toggle("active");    
-    // if (questions.classList.contains("active")) { 
-    //     boxIllustration.classList.add("move-box")
-    // } else {
-    //     boxIllustration.classList.remove("move-box")
-    // }
     // To show and hide answers on FAQs
-    this.classList.toggle("active"); // To hide other answers when one question is clicked
+    this.classList.toggle("active"); // Box moves to left when question is clicked
+
+    boxIllustration.classList.toggle("move-box"); // To hide other answers when one question is clicked
 
     if (questions.classList.contains("active")) {
       accordionQuestions.forEach(function (questions) {
         questions.classList.remove("active");
+        boxIllustration.classList.remove("move-box");
       });
       questions.classList.toggle("active");
+      boxIllustration.classList.toggle("move-box");
     }
   });
 });
@@ -168,7 +165,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44837" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32897" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ accordionQuestions.forEach(questions => {
         
         // To show and hide answers on FAQs
         this.classList.toggle("active");
+        // Box moves to left when question is clicked
+        boxIllustration.classList.toggle("move-box")
 
         // To hide other answers when one question is clicked
         if(questions.classList.contains("active")){
             accordionQuestions.forEach(function(questions){
                 questions.classList.remove("active");
+                boxIllustration.classList.remove("move-box")
             })
             questions.classList.toggle("active");
+            boxIllustration.classList.toggle("move-box")
             }   
         })
         
