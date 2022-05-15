ae04fd3ac4a70c8d9cb35b4b4d2f07cc3df175fc
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-05-15 20:49:25 +0800

    Change main margin's unit from % to px

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33461" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42541" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
